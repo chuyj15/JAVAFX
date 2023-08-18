25ecaf259a16d3b6235fd1a6cfba0a87d2080e07
chuyj15 <dlwlsla15@gmail.com>
2023-08-18 18:52:54 +0900

    123

---
+++ JavaFX_Communication/src/application/SubController.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class SubController {
+	
+	@FXML
+	TextArea areaName;
+	
+	public void printName(String name) {
+		areaName.setText(name);
+	}
+	
+	
+}
